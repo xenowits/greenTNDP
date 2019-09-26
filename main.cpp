52ec17dec87fccdffbf24e2faddce17dba69c0b0
dB2510 <dhruvbodani2510@gmail.com>
2019-09-26 23:31:29 +0530

    first_phase_done

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 #define MAXN (int)1e4+5
+#define pi pair<int,int>
 
 //graph G(original graph) and G'(residual graph)
 //first element of pair represents destination
@@ -25,34 +26,84 @@ struct demand {
 
 };
 
-// vector<int> bfs(int src,int dest)
-// {
-//   vector<int>shortest_path;
-//   bool visited[MAXN];
-//   memset(visited,false,sizeof(visited));
-//   queue<int>q;
-//   q.push(src);
-//   visited[src]=true;
-//   shortest_path.push_back(src);
-//   while(!q.empty())
-//   {
-//     int u=q.front();
-//     q.pop();
-//     for(auto v:original_graph[src])
-//     {
-//       if(!visited[v])
-//       {
-//         shortest_path.push_back(v);
-//         visited[v]=true;
-//         q.push(v);
-//         if(v==dest)
-//         return shortest_path;
-//       }
-//     }
-//   }
-// }
+void shortestPath(vector<int> &path, int u, vector<int> const& parent)
+{
+  //base case : if u is the Source
+  if (parent[u] == -1)
+  {
+    path.push_back(u);
+    return;
+  }
 
-vector<vector<int>> find_TNDP(demand d)
+  shortestPath(path, parent[u], parent);
+
+  path.push_back(u);
+
+}
+
+//Returns shortest path from src to dest
+
+vector<int> dijkstra(int V, int src, int dest, vector<pi> graph[])
+{
+    // Create a priority queue to store vertices that
+    // are being preprocessed.
+
+    priority_queue< pi, vector <pi> , greater<pi> > pq;
+
+    // Create a vector for distances and initialize all
+    // distances as infinite (MAXN)
+    vector<int> dist(V+1, MAXN);
+
+    // Insert source itself in priority queue and initialize
+    // its distance as 0.
+    pq.push(make_pair(0, src));
+    dist[src] = 0;
+
+    vector<int> parent(V+1,-1);
+
+    /* Looping till priority queue becomes empty (or all
+      distances are not finalized) */
+    while (!pq.empty())
+    {
+        // The first vertex in pair is the minimum distance
+        // vertex, extract it from priority queue.
+        // vertex label is stored in second of pair (it
+        // has to be done this way to keep the vertices
+        // sorted distance (distance must be first item
+        // in pair)
+        int u = pq.top().second;
+        pq.pop();
+
+        // 'i' is used to get all adjacent vertices of a vertex
+        // list< pair<int, int> >::iterator i;
+        for (auto x : graph[u])
+        {
+            // Get vertex label and weight of current adjacent
+            // of u.
+            int v = x.first;
+            int weight = x.second;
+
+            //  If there is shorted path to v through u.
+            if (dist[v] > dist[u] + weight)
+            {
+                // Updating distance of v
+                dist[v] = dist[u] + weight;
+                //making u as parent of v
+                parent[v] = u;
+                pq.push(make_pair(dist[v], v));
+            }
+        }
+    }
+
+    vector<int> path;
+
+    shortestPath(path, dest, parent);
+    return path;
+
+}
+
+
+vector<vector<int>> find_TNDP(int V,vector<pair<int,int>>*res, demand d)
 {
   vector<vector<int>> output;
   int s=d.s;
@@ -60,8 +111,22 @@ vector<vector<int>> find_TNDP(demand d)
   for(int i=0;i<2;i++)
   {
     vector<int>path;
-
+    path=dijkstra(V, s, t, res);
+    output.push_back(path);
+    vector<pair<int,int>>::iterator it;
+    for(int j=1;j<=V;j++)
+    {
+      for(it = res[j].begin(); it != res[j].end(); it++)
+      {
+        for(auto k:path)
+        {
+          if(k==(*it).first && (k!=s || k!=t))
+          res[j].erase(it);
+        }
+      }
+    }
   }
+  return output;
 }
 
 int main()
@@ -79,9 +144,9 @@ int main()
     cin >> u >> v >> w;      //u = source node, v = destination node
 
     original_graph[u].emplace_back(v,w);
-
     residual_graph[u].emplace_back(v,w);
   }
+
 
   int D;  //no of demands
   cin >> D;
@@ -102,7 +167,43 @@ int main()
     return a.h > b.h;
   });
 
+  vector<vector<int>> final_paths;
+  for (auto d:demands) {
+    int h=d.h;
+    vector<pair<int,int>>res[MAXN] = residual_graph;
+    for(int i=1;i<=V;i++)
+    {
+      vector<pair<int,int> >::iterator it;
+      for(it = res[i].begin(); it != res[i].end(); it++)
+      {
+        if((*it).second <= h)
+        res[i].erase(it);
+      }
+    }
+    vector<vector<int>> paths = find_TNDP(V,res,d);
+    if(paths.size()==2)
+    {
+      int path_1_sz = paths[0].size(), path_2_sz = paths[1].size();
+      for (int i = 0 ; i < path_1_sz-1; ++i)
+      {
+        auto u = residual_graph[paths[0][i]];
+        auto v = paths[0][i+1];
+        for (auto k : u)
+        {
+          if (k.first == v)
+            k.second-=h;
+        }
+      }
+    }
+    else
+      break;
+  }
 
 
   return 0;
 }
+
+	
+	
+	
+
